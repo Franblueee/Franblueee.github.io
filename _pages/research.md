539719131806b4b1bacde13241dcffd1bb087def
Franblueee <francastro8b@gmail.com>
2026-06-13 19:47:33 +0200

    update

---
+++ _pages/research.md
@@ -0,0 +1,18 @@
+---
+layout: page
+permalink: /research/
+title: Research
+description:
+nav: true
+nav_order: 2
+---
+
+<!-- _pages/research.md (was publications.md) -->
+
+{% include bib_search.liquid %}
+
+<div class="research">
+
+{% bibliography %}
+
+</div>
--- assets/js/common.js
+++ assets/js/common.js
@@ -19,8 +19,8 @@ $(document).ready(function () {
 
   // bootstrap-toc
   if ($("#toc-sidebar").length) {
-    // remove related publications years from the TOC
-    $(".publications h2").each(function () {
+    // remove related research years from the TOC
+    $(".research h2").each(function () {
       $(this).attr("data-toc-skip", "");
     });
     var navSelector = "#toc-sidebar";
